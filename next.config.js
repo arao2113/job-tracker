eeaec0266c6e3f01563e1694b42dbaf9de615b1d
Ara Pirijanyan <arao2113@gmail.com>
2022-02-10 15:17:26 -0800

    untrack config file

--- next.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  env: {
-    MONGO_URI:
-      "mongodb+srv://arao2113:vvDfidAZKfKG2jtC@cluster0.s6f9p.mongodb.net/job-tracker?retryWrites=true&w=majority",
-  },
-};
